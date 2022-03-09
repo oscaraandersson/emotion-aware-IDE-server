33223f513b486f79585d1a3631f9d04e2657afb0
Jimmy Berglund <jimmy_berglund@hotmail.com>
2022-03-09 15:01:49 +0100

    Working webview for the website

--- extension.js
+++ extension.js
@@ -1,5 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+//const { once } = require('events');
 const vscode = require('vscode');
 
 // this method is called when your extension is activated
@@ -17,15 +18,71 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('emotionawareide.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('emotionawareide.start_dashboard', () => {
 		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from EmotionAwareIDE!');
+		const panel = vscode.window.createWebviewPanel(
+			"dashboard",
+			"Dashboard",
+			vscode.ViewColumn.One,
+			{
+				enableScripts: true
+			}
+		);
+
+		panel.webview.html = getWebviewContent();
 	});
 
 	context.subscriptions.push(disposable);
 }
+
+
+function getWebviewContent() {
+	return `<!DOCTYPE html>
+	<html>
+	  
+	<head>
+		<title>full screen iframe</title>
+		<style type="text/css">
+			html {
+				overflow: auto;
+			}
+			  
+			html,
+			body,
+			div,
+			iframe {
+				margin: 0px;
+				padding: 0px;
+				height: 100%;
+				border: none;
+			}
+			  
+			iframe {
+				display: block;
+				width: 100%;
+				border: none;
+				overflow-y: auto;
+				overflow-x: hidden;
+			}
+		</style>
+	</head>
+	  
+	<body>
+		<iframe src="http://localhost:8050"
+				frameborder="0" 
+				marginheight="0" 
+				marginwidth="0" 
+				width="100%" 
+				height="100%" 
+				scrolling="auto">
+	  </iframe>
+	  
+	</body>
+	  
+	</html>`;
+}
+
 
 // this method is called when your extension is deactivated
 function deactivate() {}
