e30d174c273413edebd3f5aab73b6d1de40702b4
Oscar Andersson <oscar.966484@gmail.com>
2022-03-19 20:33:49 +0100

    compleated feature extraction

--- machine_learning/E4model.py
+++ machine_learning/E4model.py
@@ -1,54 +1,24 @@
 import torch
+import os
 
 import numpy as np
-from feature_extraction import FeatureExtractor
-from train import Model
+from .train import Model
+import sys
+import feature_extraction
 
+dirname = os.path.dirname(os.path.realpath(__file__))
 class E4model():
-    def __init__(self, baseline):
-        self.fe = FeatureExtractor(baseline)
+    def __init__(self, baseline_values):
+        self.fe = feature_extraction.FeatureExtractor(baseline_values)
 
         self.arousal_model = Model()
-        self.arousal_model.load_state_dict(torch.load('models/arousal.pth'))
+        self.arousal_model.load_state_dict(torch.load(dirname + '/models/arousal.pth'))
 
         self.valence_model = Model()
-        self.valence_model.load_state_dict(torch.load('models/valence.pth'))
-
-    def create_instance(self, HR, EDA, TEMP, BVP):
-        # creating an instance form 10 seconds of raw data
-        instance = []
-                         
-        self.fe.set_signal(EDA)
-        instance.append(self.fe.get_mean_SCL())
-        instance.append(self.fe.get_AUC_Phasic())
-        instance.append(self.fe.get_min_peak_amplitude())
-        instance.append(self.fe.get_max_peak_amplitude())
-        instance.append(self.fe.get_mean_phasic_peak())
-        instance.append(self.fe.get_sum_phasic_peak_amplitude())
+        self.valence_model.load_state_dict(torch.load(dirname + '/models/valence.pth'))
         
-        self.fe.set_signal(TEMP)
-        instance.append(self.fe.get_mean_temp())
-        instance.append(self.fe.get_mean_temp_difference())
-        instance.append(self.fe.get_max_temp())
-        instance.append(self.fe.get_max_temp_difference())
-        instance.append(self.fe.get_min_temp())
-        instance.append(self.fe.get_min_temp_difference())
-
-        self.fe.set_signal(BVP)
-        instance.append(self.fe.get_difference_BVPpeaks_ampl())
-        instance.append(self.fe.get_mean_BVPpeaks_ampl())
-        instance.append(self.fe.get_min_BVPpeaks_ampl())
-        instance.append(self.fe.get_max_BVPpeaks_ampl())
-        instance.append(self.fe.get_sum_peak_ampl())
-
-        self.fe.set_signal(HR)
-        instance.append(self.fe.get_HR_mean_difference())
-        instance.append(self.fe.get_HR_variance_difference())
-
-        return instance
-    
-        
-    def predict(self, instance):
+    def predict(self, signal_values):
+        instance = self.fe.create_instance(signal_values)
         arousal_prediction = self.arousal_model(torch.tensor(instance, dtype=torch.float32))
         valence_prediction = self.valence_model(torch.tensor(instance, dtype=torch.float32))
         if int(arousal_prediction) == 1:
