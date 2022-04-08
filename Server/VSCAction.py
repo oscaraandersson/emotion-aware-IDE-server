be95d2e13c34d55fe576662b461b7aa344a29a4c
Jimmy Berglund <jimmy_berglund@hotmail.com>
2022-04-08 10:34:56 +0200

    Fixed relative path

--- Server/VSCAction.py
+++ Server/VSCAction.py
@@ -167,7 +167,7 @@ class EstimatedEmotion(Action):
     
     def _save_prediction(self, index):
         # Check if csv file already exists
-        FILE_NAME = "../../Dashboard/emotions.csv"
+        FILE_NAME = "../Dashboard/emotions.csv"
         df = None
 
         if os.path.exists(FILE_NAME):
